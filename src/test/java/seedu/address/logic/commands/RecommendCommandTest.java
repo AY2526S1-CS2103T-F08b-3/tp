5344e59e0c3c14c8bf45228b40e503c23ae451e9
katrinaykt <katrinayeo@hotmail.com>
2025-10-14 19:39:35 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/RecommendCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RecommendCommandTest.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SIXTH_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Collections;

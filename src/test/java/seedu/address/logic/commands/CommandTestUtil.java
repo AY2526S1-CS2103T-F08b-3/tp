ea55b7a5ec718c794a8c77679dcc937cefebd601
Lee Ray Kye <e1337578@u.nus.edu>
2025-10-14 22:37:52 +0800

    Modify Edit Command Test

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -42,12 +42,13 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
-    public static final String VALID_SUBJECT_AMY = "science";
-    public static final String VALID_SUBJECT_BOB = "science";
+    public static final String VALID_SUBJECT_AMY = "Science";
+    public static final String VALID_SUBJECT_BOB = "Science";
     public static final String VALID_LEVEL_AMY = "1-3";
     public static final String VALID_LEVEL_BOB = "1-3";
     public static final String VALID_PRICE_AMY = "10-20";
     public static final String VALID_PRICE_BOB = "10-20";
+
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;

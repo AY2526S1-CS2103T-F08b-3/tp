905a14179ddcbba59de90dc328fb5ec5c6036348
tevyntan <tevyntanyb@gmail.com>
2025-10-21 22:51:14 +0800

    Change import statements for SessionAddCommand.java

--- src/main/java/seedu/address/logic/commands/SessionAddCommand.java
+++ src/main/java/seedu/address/logic/commands/SessionAddCommand.java
@@ -4,7 +4,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import seedu.address.model.session.Session;
+import seedu.address.model.person.Session;
 
 /**
  * Adds a tutoring session for a matched tutor-student pair.

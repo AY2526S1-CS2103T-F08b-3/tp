74ef7c3c13841ad18b587b06aab79a01cb89df42
Jun Wei <156562637+junwei07@users.noreply.github.com>
2025-10-28 17:14:35 +0800

    Add parser for help command to only accept no parameter

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -85,7 +85,7 @@ public class AddressBookParser {
             return new ExitCommand();
 
         case HelpCommand.COMMAND_WORD:
-            return new HelpCommand();
+            return new HelpCommandParser().parse(arguments);
 
         case MatchCommand.COMMAND_WORD:
             return new MatchCommandParser().parse(arguments);

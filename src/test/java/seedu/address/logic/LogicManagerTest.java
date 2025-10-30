110634b96982b5fe144ee025b3b7832f7d762a02
Jun Wei <156562637+junwei07@users.noreply.github.com>
2025-10-30 23:58:34 +0800

    Resolve build issue

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -71,7 +71,7 @@ public class LogicManagerTest {
     @Test
     public void execute_validCommand_success() throws Exception {
         String listCommand = ListCommand.COMMAND_WORD + " tutors";
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS_TUTORS, model);
+        assertCommandSuccess(listCommand, ListCommand.MESSAGE_EMPTY_LIST, model);
     }
 
     @Test
